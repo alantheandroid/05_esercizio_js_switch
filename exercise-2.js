5078ebc43b12acbc8c8b9c0679640ea2b42e9cb2
alantheandroid <alessio.ilardi@gmail.com>
2022-05-15 13:20:09 +0200

    feat: add 'switch' statements

--- exercise-2.js
+++ exercise-2.js
@@ -1,17 +1,29 @@
-// ceo => 2200
-// manager => 1800
-// cto => 1800
-// developer => 1500
-// default => 1000
+let ceo = 2200;
+let manager = 1800;
+let cto = 1800;
+let developer = 1500;
+let other = 1000;
+
 function calculateSalary(role) {
-  // ...
+  switch (role) {
+    case "ceo":
+      return ceo;
+    case "manager":
+      return manager;
+    case "cto":
+      return cto;
+    case "developer":
+      return developer;
+    default:
+      return other;
+  }
 }
 
-const ceoSalary = calculateSalary('ceo');
-const managerSalary = calculateSalary('manager');
-const ctoSalary = calculateSalary('cto');
-const developerSalary = calculateSalary('developer');
-const otherSalary = calculateSalary('other');
+const ceoSalary = calculateSalary("ceo");
+const managerSalary = calculateSalary("manager");
+const ctoSalary = calculateSalary("cto");
+const developerSalary = calculateSalary("developer");
+const otherSalary = calculateSalary("other");
 
 console.log(ceoSalary);
 console.log(managerSalary);
